4c25cb19d51b57f641ca43e1350335b249615e0a
PetoLau <laurinec.peter@gmail.com>
2018-03-04 19:07:53 +0100

    #2 Update: more tests based on covr

---
+++ tests/testthat/test_matrix.R
@@ -0,0 +1,19 @@
+context("Tests for repr_matrix() function");
+
+# dimension of a matrix
+data("elec_load")
+win_size <- 48
+test_that("Test on elec_load, dimension of output from repr_matrix() function", {
+  expect_equal(dim(repr_matrix(elec_load, func = repr_feaclip, windowing = TRUE, win_size = win_size)),
+                c(nrow(elec_load), 8*ncol(elec_load)/win_size))
+  expect_equal(dim(repr_matrix(elec_load, func = repr_feaclip)),
+               c(nrow(elec_load), 8))
+  expect_equal(dim(repr_matrix(elec_load, func = repr_feaclip, normalise = T, func_norm = norm_z)),
+               c(nrow(elec_load), 8))
+})
+
+# Test errors
+test_that("Test on elec_load, errors on repr_matrix() function", {
+  expect_error(repr_matrix(elec_load, func = repr_feaclip, windowing = TRUE), "win_size must be specified!")
+  expect_error(repr_matrix(elec_load), "func must be specified!")
+})
--- tests/testthat/test_pip_pla_sax.R
+++ tests/testthat/test_pip_pla_sax.R
@@ -11,11 +11,24 @@ test_that("Test on x_ts, length of output from selected repr_...() functions", {
   expect_length(repr_sax(x_ts, q = q, a = a), length(x_ts)/q)
 })
 
+# SAX test on no variance vector
+test_that("Test on no variance vector output of repr_sax() functions", {
+  expect_equal(length(unique(repr_sax(rep(0,100), q = q, a = a))), 1)
+})
+
 # Extracted values (repr.) testing
 test_that("Test on x_ts, extracted values from repr_...() functions", {
   expect_equal(repr_pla(x_ts, times = times), c(x_ts[1], rep(max(x_ts), times)))
   expect_equal(repr_pip(x_ts, times = times)[1], x_ts[1])
   expect_equal(repr_pip(x_ts, times = times)[times+1], tail(x_ts, 1))
+})
+
+# Class of output
+test_that("Test on x_ts output class from repr_...() functions", {
+  expect_true(is.numeric(repr_pla(x_ts, times = times, return = "places")))
+  expect_true(is.numeric(repr_pip(x_ts, times = times, return = "places")))
+  expect_true(is.data.frame(repr_pla(x_ts, times = times, return = "both")))
+  expect_true(is.data.frame(repr_pip(x_ts, times = times, return = "both")))
 })
 
 # Test errors
