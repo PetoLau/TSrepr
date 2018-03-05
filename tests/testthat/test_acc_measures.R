86ea775d1e37de3a9e8e28c08c49b1be2f5986e1
PetoLau <laurinec.peter@gmail.com>
2018-03-05 12:49:01 +0100

    Update: tests for src/ functions

---
+++ tests/testthat/test_acc_measures.R
@@ -0,0 +1,24 @@
+context("Tests for for. acc. measures functions");
+
+# Lengths of outputted values
+real <- rep(1:8, 12)
+forec <- rep(8:1, 12)
+naive <- rep(9:2, 12)
+
+test_that("Test on real and forec, length of output from selected functions", {
+  expect_length(mae(real, forec), 1)
+  expect_length(rmse(real, forec), 1)
+  expect_length(mdae(real, forec), 1)
+  expect_length(mape(real, forec), 1)
+  expect_length(smape(real, forec), 1)
+  expect_length(mase(real, forec, naive), 1)
+})
+
+# Extracted values
+test_that("Test on real and forec, outputted values from acc. measures functions", {
+  expect_equal(mae(c(4,5), c(2,4)), 1.5)
+  expect_equal(mdae(c(4,5), c(2,4)), 1.5)
+  expect_equal(mape(c(4,5), c(2,4)), 35)
+  # expect_equal(smape(c(4,5), c(2,4)), 11111/250)
+  expect_equal(mase(c(4,5), c(2,4), c(10,8)), 1/3)
+})
---
+++ tests/testthat/test_helpers.R
@@ -0,0 +1,20 @@
+context("Tests for for. helper functions");
+
+# Lengths of outputted values
+x_ts <- rep(1:8, 12)
+test_that("Test on x_ts, length of output from selected functions", {
+  expect_length(meanC(x_ts), 1)
+  expect_length(medianC(x_ts), 1)
+  expect_length(maxC(x_ts), 1)
+  expect_length(minC(x_ts), 1)
+  expect_length(sumC(x_ts), 1)
+})
+
+# Extracted values
+test_that("Test on x_ts, outputted values from helper functions", {
+  expect_equal(meanC(x_ts), mean(x_ts))
+  expect_equal(medianC(x_ts), median(x_ts))
+  expect_equal(sumC(x_ts), sum(x_ts))
+  expect_equal(minC(x_ts), min(x_ts))
+  expect_equal(maxC(x_ts), max(x_ts))
+})
