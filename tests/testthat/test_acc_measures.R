87ce8a004648bf2a06be76e9bf370f05259e63ef
PetoLau <laurinec.peter@gmail.com>
2018-12-06 15:15:58 +0100

    Test MSE

--- tests/testthat/test_acc_measures.R
+++ tests/testthat/test_acc_measures.R
@@ -6,6 +6,7 @@ forec <- rep(8:1, 12)
 naive <- rep(9:2, 12)
 
 test_that("Test on real and forec, length of output from selected functions", {
+  expect_length(mse(real, forec), 1)
   expect_length(mae(real, forec), 1)
   expect_length(rmse(real, forec), 1)
   expect_length(mdae(real, forec), 1)
@@ -18,6 +19,7 @@ test_that("Test on real and forec, length of output from selected functions", {
 # Extracted values
 test_that("Test on real and forec, outputted values from acc. measures functions", {
   expect_equal(mae(c(4,5), c(2,4)), 1.5)
+  expect_equal(mse(c(4,5), c(2,4)), 2.5)
   expect_equal(mdae(c(4,5), c(2,4)), 1.5)
   expect_equal(mape(c(4,5), c(2,4)), 35)
   expect_equal(maape(0, 1), pi/2)
